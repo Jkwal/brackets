e9bf03a93e3644f024b8c3e0e880219fb4588de0
Yauheny Vial <mr.jk1629003@gmail.com>
2020-10-18 22:49:48 +0300

    feat: the solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let strBrackets = bracketsConfig.map((element) => element.join(''));
+
+    for (let i = 0; i < strBrackets.length; i++) {
+        if (str.includes(strBrackets[i]) == true) {
+            str = str.replace(strBrackets[i], '');
+            i = -1;
+        }
+    }
+    return Object.is(str, '');
 }
